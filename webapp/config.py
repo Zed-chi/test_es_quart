50f6a3a7be8f2b553f518954ec93e50d83079097
zed-chi <zed-chi@mail.ru>
2023-03-25 21:00:15 +0300

    basic config

---
+++ webapp/config.py
@@ -0,0 +1,10 @@
+class Config:
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'secret'
+
+class Development(Config):
+    DEBUG = True
+
+class Production(Config):
+    SECRET_KEY = 'an actually secret key'
