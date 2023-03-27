22fa9ae7a107c26fd4c5eb79ec22e08685c36d5d
zed-chi <zed-chi@mail.ru>
2023-03-27 16:59:01 +0300

    moved views to folders

--- webapp/app.py
+++ webapp/app.py
@@ -1,78 +1,45 @@
 import logging
 
+from apps.api.views import blueprint as api_bp
+from apps.main.views import blueprint as main_bp
+from db.models import Document
 from elasticsearch import AsyncElasticsearch
 from environs import Env
-from quart import Quart, redirect, render_template, request
-from tortoise import Tortoise
-
 from init_scripts import run_init_check
-from models import Document
+from quart import Quart
+from quart_schema import QuartSchema
+from tortoise import Tortoise
 
 env = Env()
 env.read_env()
-
-logging.basicConfig(level=logging.DEBUG)
 es = None
 
-app = Quart(
-    __name__,
-    template_folder="./_templates",
-    static_folder="_static",
-    static_url_path="/static",
-)
-app.config.from_prefixed_env("APP")
 
-
-@app.route("/")
-async def index():
-    return await render_template("index.html")
-
-
-@app.post("/search")
-async def search():
-    form = await request.form
-    query = form["text"]
-    doc_ids = await get_doc_ids_by_query(query)
-    docs = await Document.filter(id__in=doc_ids).order_by("created_date")
-    serialized_docs = serialize_docs(docs)
-
-    return await render_template(
-        "index.html", query=query, results=serialized_docs
+def create_app():
+    app = Quart(
+        __name__,
+        template_folder="./_templates",
+        static_folder="_static",
+        static_url_path="/static",
     )
+    app.config.from_prefixed_env("APP")
+    app.config["DEBUG"] = env.bool("APP_DEBUG")
+    app.store = {"es": None}
+
+    app.register_blueprint(main_bp, url_prefix="/")
+    app.register_blueprint(api_bp, url_prefix="/api")
+
+    QuartSchema(app)
+
+    if app.config["DEBUG"]:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    return app
 
 
-def serialize_docs(docs):
-    return [
-        {"id": doc.id, "text": doc.text, "date": doc.created_date}
-        for doc in docs
-    ]
-
-
-async def get_doc_ids_by_query(query):
-    res = await es.search(
-        index=app.config["ES_INDEX"],
-        size=20,
-        body={"query": {"match": {"text": query}}},
-    )
-    return [i["_source"]["doc_id"] for i in res["hits"]["hits"]]
-
-
-@app.post("/delete/<int:doc_id>")
-async def delete_document(doc_id):
-    db_document = await Document.get_or_none(id=doc_id)
-    if db_document:
-        db_document.delete()
-
-    es_doc = await es.search(
-        index=app.config["ES_INDEX"],
-        size=1,
-        body={"query": {"match": {"doc_id": doc_id}}},
-    )
-    if es_doc["hits"]["hits"]:
-        _id = es_doc["hits"]["hits"][0]["_id"]
-        await es.delete(index=app.config["ES_INDEX"], id=_id)
-
-    return redirect("/")
+app = create_app()
 
 
 @app.after_serving
@@ -84,9 +51,12 @@ async def app_shutdown():
 @app.before_serving
 async def app_start():
     global es
+    app.logger.debug(app.config)
     if app.config["DB"] == "SQLITE":
+        app.logger.debug("=== Choosed SQLITE")
         db_url = f"sqlite://{app.config['SQLITE_PATH']}"
     elif app.config["DB"] == "PG":
+        app.logger.debug("=== Choosed PG")
         db_url = "postgres://{}:{}@{}:{}/{}".format(
             app.config["PG_USERNAME"],
             app.config["PG_PASS"],
@@ -97,12 +67,15 @@ async def app_start():
     else:
         raise ValueError("Please provide correct db type in ENV")
 
-    es = AsyncElasticsearch(
+    app.store["es"] = AsyncElasticsearch(
         f"http://{app.config['ES_HOST']}:{app.config['ES_PORT']}"
     )
-    await Tortoise.init(db_url=db_url, modules={"models": ["models"]})
+    await Tortoise.init(db_url=db_url, modules={"models": ["db.models"]})
     await Tortoise.generate_schemas()
-    await run_init_check(Document, es, es_index=app.config["ES_INDEX"])
+
+    if app.config["INIT_CONTENT_CHECK"] == "True":
+        app.logger.debug("=== Init Content Check")
+        await run_init_check(Document, es, es_index=app.config["ES_INDEX"])
 
 
 if __name__ == "__main__":
